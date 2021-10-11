a325bfd3d4349e55017ceaf66da767e2dba67fef
Vishal <yash.codedrill@gmail.com>
2021-10-11 17:38:15 +0530

    add second count

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React,{useReducer} from 'react'
+
 import './App.css';
 
 const initialState ={
-  firstCounter:0
+  firstCounter:0,
+  secoundCounter:10
 }
 const reducer = (state,action)=>{
   switch(action.type){
     case 'increment':
-      return {firstCounter:state.firstCounter+action.value}
+      return {...state,firstCounter:state.firstCounter+action.value}
       case 'decrement':
-        return {firstCounter:state.firstCounter-action.value }
+        return {...state,firstCounter:state.firstCounter-action.value }
         case 'reset':
         return initialState
         case'increment by 5':
-        return{firstCounter:state.firstCounter+action.value}
+        return{...state,firstCounter:state.firstCounter+action.value}
         case'decrement by 5':
-        return{firstCounter:state.firstCounter-action.value}
+        return{...state,firstCounter:state.firstCounter-action.value}
+        case 'increment2':
+          return {...state,secoundCounter:state.secoundCounter +action.value}
+          case 'decrement2':
+            return {...state,secoundCounter:state.secoundCounter + action.value}
         default:
           return state
   }
@@ -26,11 +32,14 @@ function App() {
 
   return (
     <div className="App">
-    <h3>Count - {count.firstCounter}</h3>
+    <h3>FirstCount - {count.firstCounter}</h3>
+    <h3>SecondCount - {count.secoundCounter}</h3>
      <button onClick={()=>dispatch({type:'increment',value:1})}>Increment</button>
      <button onClick={()=>dispatch({type:'decrement',value:1})}>Decrement</button>
      <button onClick={()=>dispatch({type:'increment by 5',value:5})}>Increment by 5</button>
      <button onClick={()=>dispatch({type:'decrement by 5',value:5})}>Decrement by 5</button>
+     <button onClick={()=>dispatch({type:'increment2',value:1})}>Increment2</button>
+     <button onClick={()=>dispatch({type:'decrement2',value:1})}>Decrement2</button>
      <button onClick={()=>dispatch({type:'reset'})}>Reset</button>
     </div>
   );
